23ba16588a47da6c8991d4bc149693563d66fd5d
Victor Grigoriu <vgrigoriu@gmail.com>
2015-11-02 21:58:00 +0200

    First version: read book titles from db

---
+++ CalibreBrowser/Program.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+using System;
+using System.Threading.Tasks;
+
+namespace CalibreBrowser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Query().Wait();
+
+        }
+
+        private static async Task Query()
+        {
+            var c = new SQLiteAsyncConnection(@"C:\Users\victor\calibre\metadata.db");
+
+            var books = await c.Table<books>().ToListAsync();
+
+            foreach (var book in books)
+            {
+                Console.WriteLine($"{book.id}: {book.title}");
+            }
+        }
+    }
+
+    public class books
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+    }
+}
